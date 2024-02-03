6a211487824457563801cc448dba8f860a90c80f
MaxSlipchuk <nice.slipchukmax@gmail.com>
2024-02-03 15:06:13 +0200

    new_project

---
+++ cost_accouting/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CostAccoutingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cost_accouting'
---
+++ creating_calculation/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CreatingCalculationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'creating_calculation'
